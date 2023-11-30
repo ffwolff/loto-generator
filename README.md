8da3f97af1e1431ff276454c456d60f81226a402
ffwolff <francofwolff@gmail.com>
2023-11-30 13:54:53 -0300

    create app class and the entire program

---
+++ README.md
@@ -0,0 +1,7 @@
+# Loto Generator
+
+A Java application for generating lottery numbers.
+
+## Prerequisites
+
+Install Java 17.
---
+++ src/main/App.java
@@ -0,0 +1,35 @@
+package main;
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num, i, j;
+        int[] lottery = new int[6];
+        Random random = new Random();
+        for(i=0; i<lottery.length; i++) {
+            num = random.nextInt(60) + 1;
+
+            for(j=0; j<lottery.length; j++) {
+                if(num == lottery[j] && j != i) num = random.nextInt(60) + 1;
+                else lottery[i] = num;
+            }
+        }
+
+        for(i=0; i<lottery.length; i++){
+            for(j=i+1; j<lottery.length; j++){
+                if (lottery[i] > lottery[j]){
+                    num = lottery[i];
+                    lottery[i] = lottery[j];
+                    lottery[j] = num;
+                }
+            }
+        }
+
+        for(i=0; i<lottery.length; i++){
+            System.out.print(lottery[i] + "   ");
+        }
+
+    }
+
+
+}
